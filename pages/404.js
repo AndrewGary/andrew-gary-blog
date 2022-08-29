9c6083cb18e3c1fcc6384b426a5c1abd5423636e
Andrew Gary <Andrew.gary91@gmail.com>
2022-08-29 15:59:41 -0500

    finished the 404 page with the countdown timer

--- pages/404.js
+++ pages/404.js
@@ -6,30 +6,29 @@ const NotFound = () => {
   const router = useRouter();
 
   const [countdown, setCountdown] = useState(5);
- 
 
-  const delay = (num) =>{
-    const timerArray = [5, 4, 3, 2, 1];
-    setTimeout(() => {
-      console.log(timerArray[num])
-    }, 1000)
-  }
 
   useEffect(() => {
-    for(let i = 0; i < 5; i++){
-      delay(i)
+    if(countdown !== 0){
+    setTimeout(() => {
+      setCountdown(countdown - 1);
+    }, 1000);
+    }else{
+      router.push('/')
     }
-  }, [])
+  }, [countdown])
 
   return (
     <div className='w-full h-screen flex-col flex justify-center items-center'>
+      <div className='flex flex-col justify-center items-center border border-slate-300 rounded-md w-3/5 h-3/5'>
       <div className='my-4'>
-        <h1 className='text-3xl'>Sorry but the page you are looking for does not exist</h1>
+        <h1 className='text-3xl'>Sorry, the page you are looking for does not exist</h1>
       </div>
 
       <div className='my-4'>
         {/* <h2 className='text-2xl'>Click <Link href='/'><a className=' text-blue-600'>here</a></Link> to go back to the homepage</h2> */}
-        <h2 className='text-2xl'>{`redirecting you back to the homepage in ${countdown} seconds`}</h2>
+        <h2 className='text-2xl'>Redirecting you back to the homepage in {<span className='text-red-500 font-bold'>{countdown}</span>} seconds. </h2>
+      </div>
       </div>
     </div>
   )
