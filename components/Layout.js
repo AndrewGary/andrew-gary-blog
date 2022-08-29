edb3532f78c2e8972e186dd586b09c1022e647ca
Andrew Gary <Andrew.gary91@gmail.com>
2022-08-29 16:06:39 -0500

    added Head component to the Layout.js component and applied a title to the site

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import Header from './Header';
+import Head from 'next/head';
 
 const Layout = ({ children }) => {
   return (
     <div>
+        <Head>
+          <title>Andrew Gary - Dev Blog</title>
+        </Head>
         <Header />
         { children }
     </div>
